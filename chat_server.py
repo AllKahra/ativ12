5a48391277cc1f721919b4774b3c3cfa9481d75f
Anna <annabeatrizmatosgallo@gmail.com>
2025-03-07 16:22:16 -0300

    chat_server.py

--- chat_server.py
+++ chat_server.py
@@ -15,7 +15,7 @@ def load_config():
     return config
 
 config = load_config()
-HOST = config.get('host', '127.0.0.1')
+HOST = config.get('host', '172.16.38.50')
 PORT = int(config.get('port', 12345))
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
